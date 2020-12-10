f2ea67d8da2b35dc3c3586a87ef955c0e7d87bcd
Luis_Cintron <l.cintron9966@outlook.com>
2020-12-09 19:22:57 -0500

    added map state to props in recent post, and render post on recent post

--- src/components/recentPost.js
+++ src/components/recentPost.js
@@ -8,6 +8,20 @@ class RecentPosts extends Component {
     this.props.fetchRecentPosts();
   }
 
+  renderPosts = function () {
+    const posts = this.props.recentPosts.map((post, index) => {
+      if (index < 3) {
+        return (
+          <li key={index}>
+            {post.title}
+
+          </li>
+        )
+      }
+    })
+    return posts
+  }
+
   render() {
     return (
       <div className='recent-posts'>
@@ -16,9 +30,7 @@ class RecentPosts extends Component {
             Recent Posts
               </div>
           <ul className='recent-posts__posts'>
-            <li>recent post 1</li>
-            <li>recent post 2</li>
-            <li>recent post 3</li>
+            {this.renderPosts()}
           </ul>
 
         </div>
@@ -27,5 +39,11 @@ class RecentPosts extends Component {
   }
 }
 
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+  return {
+    recentPosts: state.posts.recentPosts
+  }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
 
