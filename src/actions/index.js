c69379449160e7e6c55808a2db74ada93394af1a
Luis_Cintron <l.cintron9966@outlook.com>
2020-12-13 14:59:04 -0500

    added set results post reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SET_RECENT_POSTS } from './types'
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from './types'
 
 
 export function fetchRecentPosts() {
@@ -19,7 +19,7 @@ export function fetchPostsWithQuery(query) {
     axios.get(`https://api.dailysmarty.com/search?q=${query}`).then(response => {
       console.log('response.data.posts', response);
       dispatch({
-        type: SET_RECENT_POSTS,
+        type: SET_RESULTS_POSTS,
         payload: response.data.posts
       })
     })
