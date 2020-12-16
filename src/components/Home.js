277d695140a1f83f8119c1e817f0bcc1b9558183
Luis_Cintron <l.cintron9966@outlook.com>
2020-12-15 23:04:16 -0500

    began stying home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
   }
   render() {
     return (
-      <div>
+      <div className='home'>
         <Logo />
         <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
         <RecentPost />
