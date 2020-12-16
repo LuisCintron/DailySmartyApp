0ffd5dafabaffaec305431944eaa243ee072fa0c
Luis_Cintron <l.cintron9966@outlook.com>
2020-12-15 21:02:52 -0500

    modified actioncreator and optional callback

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from './types'
 export function fetchRecentPosts() {
   return function (dispatch) {
     axios.get('https://api.dailysmarty.com/posts').then(response => {
-      console.log('response.data.posts', response);
+
       dispatch({
         type: SET_RECENT_POSTS,
         payload: response.data.posts
@@ -14,14 +14,15 @@ export function fetchRecentPosts() {
   }
 }
 
-export function fetchPostsWithQuery(query) {
+export function fetchPostsWithQuery(query, callback) {
   return function (dispatch) {
     axios.get(`https://api.dailysmarty.com/search?q=${query}`).then(response => {
-      console.log('response.data.posts', response);
+
       dispatch({
         type: SET_RESULTS_POSTS,
         payload: response.data.posts
       })
+      callback()
     })
   }
 }
